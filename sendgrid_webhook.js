652d9c3c8725aa0c806a27a8568bfcd03ce3dbbc
w73919612 <kofecwi@gmail.com>
2019-01-04 13:58:01 -0500

    Emaily DEV works now in MediaProp Project. Will commit this to heroku and test.

--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -1,7 +1,9 @@
 var localtunnel = require('localtunnel');
-localtunnel(5000, { subdomain: 'ewcefasdrefa' }, function(err, tunnel) {
+localtunnel(5000, { subdomain: 'rbershrthdsers' }, function(err, tunnel) {
   console.log('LT running')
   console.log('TUNNEL.URL: ' + tunnel.url)
 });
+
+
 // "webhook": "forever sendgrid_webhook.js"
 // "webhook": "lt -p 5000 -s asdfavnnoxxxxh",
